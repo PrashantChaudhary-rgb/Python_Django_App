c6cf1e7e8d451895917456cc266f74e15c13701f
PrashantChaudhary-rgb <73767749+PrashantChaudhary-rgb@users.noreply.github.com>
2024-01-09 14:39:41 +0530

    getting ready for deployement

--- myproject/settings.py
+++ myproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -86,13 +87,11 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+	"default": dj_database_url.parse("postgres://banaoproject_user:n8cC17x0H4CbpSdXXPhK7Pns8uychOVf@dpg-cmegc3mn7f5s73fr578g-a.singapore-postgres.render.com/banaoproject")
 }
+
+
 
 
 # Password validation
